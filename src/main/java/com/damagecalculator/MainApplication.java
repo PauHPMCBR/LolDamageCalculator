b43f820d78f7fd1c989823e37c2eb36813d6fcf6
Pau <paumartibiosca@gmail.com>
2024-05-19 19:54:26 +0200

    Updated to 14.7

--- src/main/java/com/damagecalculator/MainApplication.java
+++ src/main/java/com/damagecalculator/MainApplication.java
@@ -56,7 +56,7 @@ public class MainApplication extends Application {
 
         controller.menuBar.prefWidthProperty().bind(stage.widthProperty());
 
-        controller.LolPatch.setText("Lol Patch: 14.6");
+        controller.LolPatch.setText("Lol Patch: 14.7");
 
         controller.evd = displayCacheThread.evd;
         controller.extraVariables.setMaxWidth(430);
